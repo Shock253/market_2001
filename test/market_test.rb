2667994d58368ab8f0ad168229c3f3399b4410c8
Ethan Hocking <eshocking253@gmail.com>
2020-02-26 10:46:10 -0700

    Add attributes to Market

--- test/market_test.rb
+++ test/market_test.rb
@@ -12,12 +12,12 @@ class MarketTest < Minitest::Test
     assert_instance_of Market, @market
   end
 
-  
+  def test_it_has_attributes
+    assert_equal "South Pearl Street Farmers Market", @market.name
+    assert_equal [], @market.vendors
+  end
 end
 
-# pry(main)> market = Market.new("South Pearl Street Farmers Market")
-# #=> #<Market:0x00007fe134933e20...>
-#
 # pry(main)> market.name
 # #=> "South Pearl Street Farmers Market"
 #
